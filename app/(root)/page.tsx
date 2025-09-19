0017dfac43203a20e24693a4583300c83d0bfe3e
FlavioRoman <romanflavio3@hotmail.com>
2025-09-19 09:19:35 -0300

    implement search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) => {
+  const query = (await searchParams).query ?? "";
   return (
     <>
       <section className="pink_container">
@@ -8,7 +15,10 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitons.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
-}
+};
+
+export default Home;
