d8e4c8cba89b7caa1ded60244057b2b4722f2194
FlavioRoman <romanflavio3@hotmail.com>
2025-09-20 19:06:24 -0300

    Implement create user

--- auth.ts
+++ auth.ts
@@ -1,6 +1,30 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { client } from "./sanity/lib/client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/query";
+import { writeClient } from "./sanity/lib/write-client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+        id: profile?.id,
+      });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id: profile?.id,
+          name: user.name,
+          username: profile?.login,
+          email: user.email,
+          image: user.image,
+          bio: profile?.bio || "",
+        });
+      }
+
+      return true;
+    },
+  },
 });
