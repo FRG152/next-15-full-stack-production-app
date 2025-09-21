5dad12585570c90593a7231a5770783737e9bb35
FlavioRoman <romanflavio3@hotmail.com>
2025-09-21 20:37:08 -0300

    Update logo dimensions in Navbar for better visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex items-center justify-between">
         <Link href={"/"}>
-          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <Image src="/logo.png" alt="logo" width={180} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
