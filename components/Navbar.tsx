7f1edf539fe41f9d488b07ff86705b727d3dbad2
FlavioRoman <romanflavio3@hotmail.com>
2025-09-21 20:32:52 -0300

    Replace header text in Navbar with logo image for improved branding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { auth, signIn, signOut } from "@/auth";
 import { LogOut, Plus } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await auth();
@@ -10,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex items-center justify-between">
         <Link href={"/"}>
-          <h1 className="text-2xl font-bold">Startups Description</h1>
+          <Image src="/logo.png" alt="logo" width={100} height={100} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
